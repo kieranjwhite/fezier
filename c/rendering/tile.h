1d2f58739df5386c552b72c54a18cf87269a24c5
Kieran J. White <kieran.white@hourglassapps.com>
2015-04-22 00:09:37 +0100

    deleted files specific to the tiles project

--- c/rendering/tile.h
+++
@@ -1,9 +0,0 @@
-#ifndef TILE_H
-#define TILE_H
-#include "draw.h"
-
-uint32 tile_render(uint32 *p_strokes, uint32 ord, uint32 left, uint32 top, uint32 w, uint32 h, float32 reduction);
-uint32 tile_size();
-void tile_reset();
-
-#endif
